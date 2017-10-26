72f815612e4a39c14ef9a1a40923a938895b840f
John Calderaio <jcalderaio@gmail.com>
2017-10-26 17:08:39 -0400

    first push

---
+++ Carpool.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Carpool {
+    public static void main(String args[]) {
+        Gas gas = new Gas();
+        gas.calculatePricePerPerson();
+    }
+}
+
+class Gas {
+
+    public void calculatePricePerPerson() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("How many passengers do you have?");
+        int pass = scan.nextInt();
+
+        System.out.println("How many miles is the round trip?");
+        double rtd = scan.nextDouble();
+
+        System.out.println("What gas mileage does your car get?");
+        double gm = scan.nextDouble();
+
+        System.out.println("What are the current gas prices?");
+        double gp = scan.nextDouble();
+
+        double cost = ((rtd/gm)*gp)/pass;
+        System.out.println("\nEach passenger should pay $" +cost);
+    }
+}
